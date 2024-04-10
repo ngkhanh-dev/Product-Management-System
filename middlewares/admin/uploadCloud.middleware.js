65ebc988da83215e4b419eed46c77c30d5239758
ngdangkhanhdev <ngdangkhanh233@gmail.com>
2024-04-10 15:53:27 +0700

    Change Cloud Account

--- middlewares/admin/uploadCloud.middleware.js
+++ middlewares/admin/uploadCloud.middleware.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const streamifier = require("streamifier");
 
 cloudinary.config({
-    cloud_name: process.env.cloud_name,
-    api_key: process.env.api_key,
-    api_secret: process.env.api_secret,
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
 });
 
 module.exports.uploadSingle = (req, res, next) => {
